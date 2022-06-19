719771b1db0eeaae79a8c64dcd67ca8ac84c987d
PieroCahuanaPalomino <piero.cahuana.69.palomino@gmail.com>
2022-06-19 13:20:53 -0500

    Eureka Inicios de loadBalanced en Package Service y Config

--- user-service/src/main/java/com/tutorial/user/service/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/tutorial/user/service/config/RestTemplateConfig.java
@@ -5,6 +5,7 @@
  */
 package com.tutorial.user.service.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }  
